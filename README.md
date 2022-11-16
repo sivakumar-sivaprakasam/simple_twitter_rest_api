5ac049768faa892e0ba709f8d26a2c6d9dc915ee
Sivakumar Sivaprakasam <write2sivakumar@gmail.com>
2022-11-16 20:07:06 +0800

    Simple twitter api implementation in Rust

---
+++ README.md
@@ -0,0 +1,76 @@
+# Simple Twitter API implementation using Rust
+
+In this sample project, I will create a small part of Twitter API to be able to post, read, and like tweets. 
+
+## Pre-requisites
+
+1. Cargo 
+2. PostgreSQL v14 or higher
+3. Environment variable `DATABASE_URL` configured with postgre database URL
+
+## Setup
+
+### PostgreSQL Table schema
+
+We need to create below tables in `test` schema in PostgreSQL DB 
+
+```
+create table test.tweets (
+id uuid not null primary key, 
+created_at timestamp not null, 
+message varchar(500)
+);
+
+create table test.likes (
+id uuid not null primary key, 
+created_at timestamp not null, 
+tweet_id uuid not null references test.tweets(id));
+```
+
+In my case, I have used custom schema named `test`. When you create environment variable, make sure you pass the schema name in the URL
+
+Ex: 
+
+```
+set DATABASE_URL=postgres://postgres:postgres@localhost:5432/testdb?options=-c%20search_path%3Dtest
+```
+
+### Launch the application
+
+Once the pre-requsites are well configured, please run `cargo run` to launch the application. You can use below CURL commands to test the application
+
+*Post a tweet*
+
+```
+curl -X POST -d "{\"message\": \"This is a tweet\"}" -H "Content-type: application/json" http://localhost:9090/tweets
+```
+
+*Retrieve all tweets*
+
+```
+curl http://localhost:9090/tweets
+```
+
+*Get a specific tweet*
+
+```
+curl http://localhost:9090/tweets/{id}
+```
+
+*Like a tweet*
+
+```
+curl -X POST http://localhost:9090/tweets/{id}/likes
+```
+
+*Unlike a tweet*
+
+```
+curl -X DELETE http://localhost:9090/tweets/{id}/likes
+```
+
+*Delete a tweet*
+
+```
+curl -X DELETE http://localhost:9090/tweets/{id}
+```
---
+++ src/tweet.rs
@@ -0,0 +1,210 @@
+use actix_web::web::{Data, Json, Path};
+use actix_web::{web, HttpResponse};
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
+use diesel::result::Error;
+use diesel::{ExpressionMethods, Insertable, Queryable, RunQueryDsl};
+use uuid::Uuid;
+
+use crate::constants::{APPLICATION_JSON, CONNECTION_POOL_ERROR};
+use crate::like::{list_likes, Like};
+use crate::response::Response;
+use crate::{DBPool, DBPooledConnection};
+use serde::{Deserialize, Serialize};
+use super::schema::tweets;
+use diesel::query_dsl::methods::{FilterDsl, LimitDsl, OrderDsl};
+use std::str::FromStr;
+
+pub type Tweets = Response<Tweet>;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Tweet {
+	pub id: String,
+	pub created_at: DateTime<Utc>,
+	pub message: String,
+	pub likes: Vec<Like>,	
+}
+
+impl Tweet {
+	pub fn new(message: String) -> Self {
+		Self {
+			id: Uuid::new_v4().to_string(),
+			created_at: Utc::now(),
+			message,
+			likes: vec![],
+		}
+	}
+	
+	pub fn to_tweet_db(&self) -> TweetDB {
+		TweetDB {
+			id: Uuid::new_v4(),
+			created_at: Utc::now().naive_utc(),
+			message: self.message.clone(),
+		}
+	}
+	
+	pub fn add_likes(&self, likes: Vec<Like>) -> Self {
+		Self {
+			id: self.id.clone(),
+			created_at: self.created_at.clone(),
+			message: self.message.clone(),
+			likes
+		}
+	}
+}
+
+#[derive(Queryable, Insertable)]
+#[diesel(table_name = tweets)]
+pub struct TweetDB {
+	pub id: Uuid,
+	pub created_at: NaiveDateTime,
+	pub message: String,
+}
+
+impl TweetDB {
+	fn to_tweet(&self) -> Tweet {
+		Tweet {
+			id: self.id.to_string(),
+			created_at: Utc.from_utc_datetime(&self.created_at),
+			message: self.message.clone(),
+			likes: vec![],
+		}
+	}
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TweetRequest {
+	pub message: Option<String>,
+}
+
+impl TweetRequest {
+	pub fn to_tweet(&self) -> Option<Tweet> {
+		match &self.message {
+			Some(message) => Some(Tweet::new(message.to_string())),
+			None => None
+		}
+	}
+}
+
+fn list_tweets(total_tweets: i64, conn: &mut DBPooledConnection) -> Result<Tweets, Error> {
+	use crate::schema::tweets::dsl::*;
+	
+	let _tweets = match tweets
+		.order(created_at.desc())
+		.limit(total_tweets)
+		.load::<TweetDB>(conn) {
+		Ok(tws) => tws,
+		Err(_) => vec![],
+	};
+	
+	Ok(Tweets {
+		results: _tweets.into_iter().map(|t| t.to_tweet()).collect::<Vec<Tweet>>(),
+	})
+}
+
+fn find_tweet(_id: Uuid, conn: &mut DBPooledConnection) -> Result<Tweet, Error> {
+	use crate::schema::tweets::dsl::*;
+	
+	let res = tweets.filter(id.eq(_id)).load::<TweetDB>(conn);
+	match res {
+		Ok(tweet_db) => match tweet_db.first() {
+			Some(tweet_db) => Ok(tweet_db.to_tweet()),
+			_ => Err(Error::NotFound),
+		},
+		Err(err) => Err(err),
+	}
+}
+
+fn create_tweet(tweet: Tweet, conn: &mut DBPooledConnection) -> Result<Tweet, Error> {
+	use crate::schema::tweets::dsl::*;
+	
+	let tweet_db = tweet.to_tweet_db();
+	let _ = diesel::insert_into(tweets).values(&tweet_db).execute(conn);
+	
+	Ok(tweet_db.to_tweet())	
+}
+
+fn delete_tweet(_id: Uuid, conn: &mut DBPooledConnection) -> Result<(), Error> {
+	use crate::schema::tweets::dsl::*;
+	
+	let lres = crate::like::delete_all_likes(_id, conn);
+	match lres {
+		Ok(_) => {
+			println!("Likes for tweet {} deleted in likes table!!!", _id);
+			let res = diesel::delete(tweets.filter(id.eq(_id))).execute(conn);
+			match res {
+				Ok(_) => {
+					println!("Tweet {} deleted in tweets table!!!", _id);
+					Ok(())
+				},
+				Err(err) => Err(err),
+			}
+		}
+		Err(err) => {
+			println!("Error while deleting tweets {:?}", err);
+			Err(err)
+		},
+	}
+}
+
+#[get("/tweets")]
+pub async fn list(pool: Data<DBPool>) -> HttpResponse {
+	let mut conn = pool.get().expect(CONNECTION_POOL_ERROR);
+	let tweets = web::block(move || list_tweets(50, &mut conn)).await.unwrap();
+	
+	match tweets {
+		Ok(mut twts) => {
+			let mut conn = pool.get().expect(CONNECTION_POOL_ERROR);
+			let tweets_with_likes = Tweets {
+				results: twts.results
+					.iter_mut()
+					.map(|t| {
+						let _likes = list_likes(Uuid::from_str(t.id.as_str()).unwrap(), &mut conn).unwrap();
+						t.add_likes(_likes.results)
+					})
+					.collect::<Vec<Tweet>>()
+			};
+			
+			HttpResponse::Ok().content_type(APPLICATION_JSON).json(tweets_with_likes)
+		},
+		Err(_) => {
+			HttpResponse::NotFound().content_type(APPLICATION_JSON).await.unwrap()
+		},
+	}
+}
+
+#[post("/tweets")]
+pub async fn create(tweet_req: Json<TweetRequest>, pool: Data<DBPool>) -> HttpResponse {
+	let mut conn = pool.get().expect(CONNECTION_POOL_ERROR);
+	
+	let tweet = web::block(move || create_tweet(tweet_req.to_tweet().unwrap(), &mut conn)).await.unwrap();
+	
+	match tweet {
+		Ok(twt) => HttpResponse::Created().content_type(APPLICATION_JSON).json(twt),
+		_ => HttpResponse::NoContent().await.unwrap(),
+	}
+}
+
+#[get("/tweets/{id}")]
+pub async fn get(path: Path<String>, pool: Data<DBPool>) -> HttpResponse {
+	let mut conn = pool.get().expect(CONNECTION_POOL_ERROR);
+	let tweet = web::block(move || find_tweet(Uuid::from_str(path.as_str()).unwrap(), &mut conn)).await.unwrap();
+	
+	match tweet {
+		Ok(tweet) => {
+			let mut conn = pool.get().expect(CONNECTION_POOL_ERROR);
+			let _likes = list_likes(Uuid::from_str(tweet.id.as_str()).unwrap(), &mut conn).unwrap();
+			
+			HttpResponse::Ok().content_type(APPLICATION_JSON).json(tweet.add_likes(_likes.results))
+		},
+		_ => HttpResponse::NoContent().content_type(APPLICATION_JSON).await.unwrap(),
+	}
+}
+
+#[delete("/tweets/{id}")]
+pub async fn delete(path: Path<String>, pool: Data<DBPool>) -> HttpResponse {
+	let mut conn = pool.get().expect(CONNECTION_POOL_ERROR);
+	
+	let _ = web::block(move || delete_tweet(Uuid::from_str(path.as_str()).unwrap(), &mut conn)).await;
+		
+	HttpResponse::NoContent().content_type(APPLICATION_JSON).await.unwrap()
+}
